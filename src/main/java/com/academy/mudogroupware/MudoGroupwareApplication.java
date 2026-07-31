6b65cc5f684e9e406018af39491a07a652fdb95b
minseo0327 <barkmin9@naver.com>
2026-07-31 18:36:58 +0900

    Initial commit: 프로젝트 초기 세팅

---
+++ src/main/java/com/academy/mudogroupware/MudoGroupwareApplication.java
@@ -0,0 +1,13 @@
+package com.academy.mudogroupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudoGroupwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudoGroupwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/academy/mudogroupware/MudoGroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.mudogroupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudoGroupwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
