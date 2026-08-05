97bd7b0c13411f28cd38241bae602a560f1b8449
juwon <sjw4690@gmail.com>
2026-08-05 19:35:49 +0900

    migration 수정

--- src/main/resources/db/migration/be2/V2.1.6__add_attendance_record_academy_and_notes.sql
+++ src/main/resources/db/migration/be2/V2.1.6__add_attendance_record_academy_and_notes.sql
@@ -1,7 +1,8 @@
 ALTER TABLE attendance_record
     ADD COLUMN academy_id BIGINT NULL AFTER user_id,
     ADD COLUMN clock_in_note VARCHAR(255) NULL AFTER clock_in_at,
-    ADD COLUMN clock_out_note VARCHAR(255) NULL AFTER clock_out_at;
+    ADD COLUMN clock_out_note VARCHAR(255) NULL AFTER clock_out_at,
+    ADD INDEX idx_attendance_record_user_id (user_id);
 
 UPDATE attendance_record ar
 JOIN users u ON u.id = ar.user_id
