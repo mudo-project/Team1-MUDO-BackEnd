f368d60035a32dfea5be159059bb38f7bfab82e2
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-08-05 12:21:16 +0900

    [FEAT] POST /api/roles 컨트롤러 추가

---
+++ src/main/java/com/academy/mudogroupware/users/presentation/api/RoleController.java
@@ -0,0 +1,39 @@
+package com.academy.mudogroupware.users.presentation.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.academy.mudogroupware.global.presentation.api.common.GlobalApiResponse;
+import com.academy.mudogroupware.global.presentation.security.AuthUser;
+import com.academy.mudogroupware.users.application.usecase.CreateRoleUseCase;
+import com.academy.mudogroupware.users.presentation.api.common.RoleResponseCode;
+import com.academy.mudogroupware.users.presentation.api.request.CreateRoleRequest;
+import com.academy.mudogroupware.users.presentation.api.response.RoleCreateResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/roles")
+@RequiredArgsConstructor
+public class RoleController {
+
+    private final CreateRoleUseCase createRoleUseCase;
+
+    @PreAuthorize("hasAuthority('ROLE:MANAGE')")
+    @PostMapping
+    public ResponseEntity<GlobalApiResponse<RoleCreateResponse>> createRole(
+            @AuthenticationPrincipal AuthUser authUser,
+            @Valid @RequestBody CreateRoleRequest request) {
+        Long roleId = createRoleUseCase.createRole(request.toCommand(authUser.academyId()));
+        RoleCreateResponse data = RoleCreateResponse.from(roleId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(GlobalApiResponse.created(RoleResponseCode.ROLE_CREATED, data));
+    }
+}
