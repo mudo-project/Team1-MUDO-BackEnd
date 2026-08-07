8e4e8245147d116d4fa965bf0a8be96943be7317
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-08-07 12:49:06 +0900

    [FEAT] AcademyApplicationRepository 구현 (findAll만)

---
+++ src/main/java/com/academy/mudogroupware/users/infrastructure/persistence/AcademyApplicationJpaRepository.java
@@ -0,0 +1,6 @@
+package com.academy.mudogroupware.users.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademyApplicationJpaRepository extends JpaRepository<AcademyApplicationEntity, Long> {
+}
